711cc345ff9a799a4c330398e827debaa2cc7ac6
verba <ilja-verba00@yandex.ru>
2024-05-20 08:26:40 +0800

    router: web history

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createMemoryHistory, createRouter } from 'vue-router'
+import {createRouter, createWebHistory} from 'vue-router'
 
 import Main from '../components/Main.vue'
 import AR from '../components/projects/ar/AugmentedReality.vue'
@@ -9,6 +9,6 @@ const routes = [
 ]
 
 export default createRouter({
-  history: createMemoryHistory(),
+  history: createWebHistory(),
   routes,
 })
